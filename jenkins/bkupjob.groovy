6bf4d60458b72675bb452dab9806e933d7d57f6e
ganesh <ganeshsgohane>
2023-01-15 14:48:01 +0530

    dev

---
+++ jenkins/bkupjob.groovy
@@ -0,0 +1,51 @@
+pipelineJob('bkup1') {
+
+  description('')
+
+  displayName('bkup1')
+
+  configure { flowdefinition ->
+
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2683.vd0a_8f6a_1c263') {
+
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+
+        'configVersion'(2)
+
+        'userRemoteConfigs' {
+
+          'hudson.plugins.git.UserRemoteConfig' {
+
+            'url'('https://github.com/ganeshgohane/devops-b-16.git')
+
+          }
+
+        }
+
+        'branches' {
+
+          'hudson.plugins.git.BranchSpec' {
+
+            'name'('*/master')
+
+          }
+
+        }
+
+        'doGenerateSubmoduleConfigurations'(false)
+
+        'submoduleCfg'(class:'empty-list')
+
+        'extensions'()
+
+      }
+
+      'scriptPath'('project-pipeline/scriptedpipeline.groovy')
+
+      'lightweight'(true)
+
+    }
+
+  }
+
+}
