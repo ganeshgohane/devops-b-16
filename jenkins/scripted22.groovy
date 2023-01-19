d8700f0bdc278c255619cb607a3a02043e6ec57b
ganesh <ganeshsgohane>
2023-01-19 14:25:23 +0530

    remo

---
+++ jenkins/scripted22.groovy
@@ -0,0 +1,14 @@
+node {  
+    stage('Pull') { 
+         git 'https://github.com/ganeshgohane/b-19.git'
+    }
+    stage('Build') { 
+       echo 'PIPELINE BUILD SUCCESSFULLY'  
+    }
+    stage('Test') { 
+        echo 'PIPELINE TEST SUCCESSFULLY'
+    }
+    stage('Deploy') { 
+        echo 'PIPELINE DEPLOY SUCCESSFULLY'
+    }
+}
